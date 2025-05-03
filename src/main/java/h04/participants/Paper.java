729f353549fc2887d08341e550280a442670958a
babdeman <80133701+babdeman@users.noreply.github.com>
2025-05-03 14:58:56 +0200

    implemented ternary operator in Paper

--- src/main/java/h04/participants/Paper.java
+++ src/main/java/h04/participants/Paper.java
@@ -2,11 +2,17 @@ package h04.participants;
 
 import fopbot.Direction;
 
+import java.util.Random;
+
 public class Paper extends Participant{
 
-
     public Paper(int x, int y) {
-        super(x, y, Direction.UP);
+        super(x, y, getOrientation() ? Direction.UP : Direction.DOWN);
+    }
+
+    private static boolean getOrientation(){
+        Random random = new Random();
+        return random.nextBoolean();
     }
 
     @Override
