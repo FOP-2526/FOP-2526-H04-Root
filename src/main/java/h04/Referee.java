f0e5ca22f67ccea1a2f0ff424898041ef3b3b469
babdeman <80133701+babdeman@users.noreply.github.com>
2025-05-04 22:46:49 +0200

    implemented initialize() in Referee

--- src/main/java/h04/Referee.java
+++ src/main/java/h04/Referee.java
@@ -1,6 +1,9 @@
 package h04;
 
+import h04.participants.Paper;
 import h04.participants.Participant;
+import h04.participants.Rock;
+import h04.participants.Scissors;
 
 public class Referee {
     Participant[] participants;
@@ -8,5 +11,24 @@ public class Referee {
 
     public Referee(int[] setup){
         this.setup = setup;
+        initialize();
     }
+
+    private void initialize(){
+        participants = new Participant[setup.length];
+        for(int i=0;i<setup.length;i++){
+            switch(setup[i]){
+                case 0 -> {
+                    participants[i] = new Rock(i, 0);
+                }
+                case 1 -> {
+                    participants[i] = new Paper(i, 0);
+                }
+                case 2 -> {
+                    participants[i] = new Scissors(i, 0);
+                }
+            }
+        }
+    }
+
 }
