e3fadad94f3da66c05685a9a4408805bfe613f6f
babdeman <80133701+babdeman@users.noreply.github.com>
2025-05-05 15:49:30 +0200

    subtypes of Participant now have different designs

--- src/main/java/h04/participants/Participant.java
+++ src/main/java/h04/participants/Participant.java
@@ -2,14 +2,27 @@ package h04.participants;
 
 import fopbot.Robot;
 import fopbot.Direction;
+import fopbot.RobotFamily;
 import h04.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Participant extends Robot {
 
+    private final static Map<Class<? extends Participant>, RobotFamily> designs = new HashMap<>();
+
     protected final fopbot.Direction orientation;
+
+    static {
+        designs.put(Rock.class, RobotFamily.SQUARE_RED);
+        designs.put(Paper.class, RobotFamily.SQUARE_BLUE);
+        designs.put(Scissors.class, RobotFamily.SQUARE_GREEN);
+    }
 
     public Participant(int x, int y, Direction orientation) {
         super(x, y);
+        setRobotFamily(designs.get(this.getClass()));
         this.orientation = orientation;
     }
 
