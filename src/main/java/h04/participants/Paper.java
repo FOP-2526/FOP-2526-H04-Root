d29af83cdb30833e0404915c4527bc98083333e3
babdeman <80133701+babdeman@users.noreply.github.com>
2025-05-09 15:56:20 +0200

    implemented doVictoryDance() in Paper

--- src/main/java/h04/participants/Paper.java
+++ src/main/java/h04/participants/Paper.java
@@ -1,13 +1,17 @@
 package h04.participants;
 
 import fopbot.Direction;
+import h04.Utils;
 
 import java.util.Random;
 
 public class Paper extends Participant{
 
+    private final int startingXPosition;
+
     public Paper(int x, int y) {
         super(x, y, determineOrientation() ? Direction.UP : Direction.DOWN);
+        startingXPosition = x;
     }
 
     private static boolean determineOrientation(){
@@ -17,7 +21,16 @@ public class Paper extends Participant{
 
     @Override
     public void doVictoryDance() {
-
+        while(!isFacingRight()){
+            turnLeft();
+        }
+        for(int i=0;i<startingXPosition;i++){
+            while(!Utils.isFacingWall(this)){
+                move();
+            }
+            turnLeft();
+            turnLeft();
+        }
     }
 
     @Override
